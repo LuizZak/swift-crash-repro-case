81aba8becdb95426ea8b48893ca1c7fa3a7dc5fe
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2019-04-28 09:08:58 -0300

    Correct the mock unit test, while we're at it

--- Sources/CSource/include/Rgba.h
+++ Sources/CSource/include/Rgba.h
@@ -14,10 +14,10 @@ struct Rgba64 {
     union {
         uint64_t value;
         struct {
-            uint32_t a : 16;
-            uint32_t r : 16;
-            uint32_t g : 16;
             uint32_t b : 16;
+            uint32_t g : 16;
+            uint32_t r : 16;
+            uint32_t a : 16;
         };
     };
 };
@@ -26,10 +26,10 @@ struct Rgba32 {
     union {
         uint32_t value;
         struct {
-            uint32_t a : 8;
-            uint32_t r : 8;
-            uint32_t g : 8;
             uint32_t b : 8;
+            uint32_t g : 8;
+            uint32_t r : 8;
+            uint32_t a : 8;
         };
     };
 };
